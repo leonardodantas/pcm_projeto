1d0f5555f036d6574c22e2fe1e75f976c7e0af3f
leonardodantas <leonardo1317@yahoo.com.br>
2019-04-29 17:18:17 -0300

    equipamentoManutenção

--- pcm/src/app/adm/home-adm/manutecao-adm/manutecao-adm.component.ts
+++ pcm/src/app/adm/home-adm/manutecao-adm/manutecao-adm.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ManutencaoEquipamento } from 'src/app/model/manutecao_equipamento';
 import { ManutencaoEquipService } from 'src/app/services/manutencao-equip.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-manutecao-adm',
@@ -11,6 +12,15 @@ import { ManutencaoEquipService } from 'src/app/services/manutencao-equip.servic
 export class ManutecaoAdmComponent implements OnInit {
 
   public manutecao_equip: ManutencaoEquipamento[]
+  public visualizarTabela: boolean
+
+  public formManutencao =  new FormGroup({
+    'id': new FormControl(null),
+    'equipamento': new FormControl(null),
+    'descricao_envio': new FormControl(null),
+    'descricao_atual': new FormControl(null),
+    'data_envio': new FormControl(null)
+  })
 
   constructor(
     private manuntecaoEquipService: ManutencaoEquipService
@@ -20,9 +30,20 @@ export class ManutecaoAdmComponent implements OnInit {
     this.manuntecaoEquipService.get().subscribe(
       (manutencao_equip:ManutencaoEquipamento[])=>{
         this.manutecao_equip = manutencao_equip
-        console.log(this.manutecao_equip)
       }
     )
   }
 
+  public visualizarManu(manutencao: ManutencaoEquipamento): void{
+    this.visualizarTabela = true
+    this.formManutencao.controls.id.setValue(manutencao.id)
+    this.formManutencao.controls.equipamento.setValue(manutencao.nome)
+    this.formManutencao.controls.data_envio.setValue(manutencao.data_envio)
+    this.formManutencao.controls.descricao_envio.setValue(manutencao.descricao_envio_user)
+    this.formManutencao.controls.descricao_atual.setValue(manutencao.descricao_atual_manu)
+  }
+
+  public fechar(): void{
+    this.visualizarTabela = false
+  }
 }
